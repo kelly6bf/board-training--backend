ad28c216e46dbceea237b3773553d6c55d3fb9e0
edgar6bf <edgar6bf@gmail.com>
2023-08-05 20:15:57 +0900

    chore: #1 Spring Boot Project 생성

---
+++ src/main/java/site/board/boardtraining/BoardTrainingApplication.java
@@ -0,0 +1,13 @@
+package site.board.boardtraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardTrainingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardTrainingApplication.class, args);
+    }
+
+}
