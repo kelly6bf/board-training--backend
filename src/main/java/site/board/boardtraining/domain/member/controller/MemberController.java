40cf7fe1bfdca76e1bb347627975a5fc7d9012a3
edgar6bf <edgar6bf@gmail.com>
2023-08-08 03:55:29 +0900

    refactor: #11 createMember 메서드 응답 구조 변경

--- src/main/java/site/board/boardtraining/domain/member/controller/MemberController.java
+++ src/main/java/site/board/boardtraining/domain/member/controller/MemberController.java
@@ -1,6 +1,7 @@
 package site.board.boardtraining.domain.member.controller;
 
 import jakarta.transaction.Transactional;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import site.board.boardtraining.domain.member.dto.CreateMemberRequest;
 import site.board.boardtraining.domain.member.dto.CreateMemberResponse;
 import site.board.boardtraining.domain.member.service.MemberService;
+import site.board.boardtraining.global.response.success.SingleSuccessApiResponse;
 
 @RequestMapping("/api/members")
 @RestController
@@ -21,9 +23,16 @@ public class MemberController {
 
     @Transactional
     @PostMapping
-    public CreateMemberResponse createMember(@RequestBody CreateMemberRequest request) {
-        return CreateMemberResponse.from(
-                memberService.createMember(request)
+    public ResponseEntity<SingleSuccessApiResponse<CreateMemberResponse>> createMember(
+            @RequestBody CreateMemberRequest request
+    ) {
+        return ResponseEntity.ok(
+                SingleSuccessApiResponse.of(
+                        "성공적으로 멤버가 생성되었습니다.",
+                        CreateMemberResponse.from(
+                                memberService.createMember(request)
+                        )
+                )
         );
     }
 }
