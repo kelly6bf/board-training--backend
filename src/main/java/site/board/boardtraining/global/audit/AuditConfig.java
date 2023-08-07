55f3f04dff4494eaef526dc288aa33a6637d2b98
edgar6bf <edgar6bf@gmail.com>
2023-08-08 04:05:31 +0900

    refactor: #6 audit 현재 인증된 사용자의 personalId를 메타데이터에 추가하도록 로직 수정

--- src/main/java/site/board/boardtraining/global/audit/AuditConfig.java
+++ src/main/java/site/board/boardtraining/global/audit/AuditConfig.java
@@ -5,15 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import java.util.Optional;
-
 @Configuration
 @EnableJpaAuditing
 public class AuditConfig {
     @Bean
     public AuditorAware<String> auditorProvider() {
-
-        // TODO: 인증 기능 도입 후 사용자 personalId로 자동 주입 되도록 리팩토링
-        return () -> Optional.of("tester");
+        return new AuditorAwareImpl();
     }
 }
