c19951b4e66ba15f1f70929c3c4a2f0d2904282a
edgar6bf <edgar6bf@gmail.com>
2023-08-08 03:24:33 +0900

    refactor: #11 LoginFailureHandler 에러 응답 로직 추가

--- src/main/java/site/board/boardtraining/domain/auth/handler/LoginFailureHandler.java
+++ src/main/java/site/board/boardtraining/domain/auth/handler/LoginFailureHandler.java
@@ -3,17 +3,42 @@ package site.board.boardtraining.domain.auth.handler;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
+import site.board.boardtraining.global.exception.ErrorCode;
+import site.board.boardtraining.global.response.error.ErrorApiResponse;
 
 import java.io.IOException;
 
+import static site.board.boardtraining.domain.auth.exception.AuthErrorCode.INVALID_CREDENTIALS;
+
+@Slf4j
 @Component
 public class LoginFailureHandler implements AuthenticationFailureHandler {
+
     @Override
-    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
-        System.out.println("LoginFailureHandler.onAuthenticationFailure");
-        response.setStatus(403);
+    public void onAuthenticationFailure(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            AuthenticationException exception
+    ) throws IOException, ServletException {
+        log.error("LoginFailureHandler.onAuthenticationFailure");
+        setResponse(
+                response,
+                INVALID_CREDENTIALS
+        );
+    }
+
+    private void setResponse(
+            HttpServletResponse response,
+            ErrorCode errorCode
+    ) throws IOException {
+        response.setContentType("application/json;charset=UTF-8");
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.getWriter().write(
+                ErrorApiResponse.of(errorCode).convertResponseToJson()
+        );
     }
 }
