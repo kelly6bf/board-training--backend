9c1021fa1a947468eaef2cbfbb00f75815bcbcbe
edgar6bf <edgar6bf@gmail.com>
2023-08-25 03:32:58 +0900

    fix: #26 ArticleCommentRepository 누락된 코드 추가

--- src/main/java/site/board/boardtraining/domain/comment/repository/ArticleCommentRepository.java
+++ src/main/java/site/board/boardtraining/domain/comment/repository/ArticleCommentRepository.java
@@ -1,4 +1,8 @@
 package site.board.boardtraining.domain.comment.repository;
 
-public interface ArticleCommentRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import site.board.boardtraining.domain.comment.entity.ArticleComment;
+
+public interface ArticleCommentRepository
+        extends JpaRepository<ArticleComment, Long> {
 }
