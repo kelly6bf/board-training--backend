a6e88f250bec3bc486154b2cfc1d06332b3c22e9
edgar6bf <edgar6bf@gmail.com>
2023-08-26 01:34:06 +0900

    feat: #26 ArticleCommentRepository 댓글 검색 및 삭제 메서드 추가

--- src/main/java/site/board/boardtraining/domain/comment/repository/ArticleCommentRepository.java
+++ src/main/java/site/board/boardtraining/domain/comment/repository/ArticleCommentRepository.java
@@ -1,8 +1,17 @@
 package site.board.boardtraining.domain.comment.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import site.board.boardtraining.domain.comment.constant.ArticleCommentType;
 import site.board.boardtraining.domain.comment.entity.ArticleComment;
+
+import java.util.List;
 
 public interface ArticleCommentRepository
         extends JpaRepository<ArticleComment, Long> {
+
+    void deleteAllByParentCommentId(Long parentCommentId);
+
+    List<ArticleComment> findAllByArticle_IdAndCommentType(Long articleId, ArticleCommentType commentType);
+
+    List<ArticleComment> findAllByParentCommentId(Long parentCommentId);
 }
