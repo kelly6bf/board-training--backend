e4564cb99d1e08903b71487258444b6f0dc3eceb
edgar6bf <edgar6bf@gmail.com>
2023-08-28 21:50:15 +0900

    refactor: #38 ArticleRepository 불필요한 로직제거 및 쿼리 메서드 변경
    ArticleRepository의 불필요한 로직을 제거하고 쿼리 메서드의 파라미터를 변경하였다.

--- src/main/java/site/board/boardtraining/domain/article/repository/ArticleRepository.java
+++ src/main/java/site/board/boardtraining/domain/article/repository/ArticleRepository.java
@@ -1,39 +1,15 @@
 package site.board.boardtraining.domain.article.repository;
 
-import com.querydsl.core.types.dsl.DateTimeExpression;
-import com.querydsl.core.types.dsl.SimpleExpression;
-import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
-import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
-import org.springframework.data.querydsl.binding.QuerydslBindings;
 import site.board.boardtraining.domain.article.entity.Article;
-import site.board.boardtraining.domain.article.entity.QArticle;
+import site.board.boardtraining.domain.board.entity.Board;
 
 public interface ArticleRepository
         extends JpaRepository<Article, Long>,
-        QuerydslPredicateExecutor<Article>,
-        QuerydslBinderCustomizer<QArticle> {
+        QuerydslPredicateExecutor<Article> {
 
-    Page<Article> findByBoard_IdAndTitleContainingOrContentContaining(Long boardId, String title, String content, Pageable pageable);
-
-    @Override
-    default void customize(
-            QuerydslBindings bindings,
-            QArticle root
-    ) {
-        bindings.excludeUnlistedProperties(true);
-        bindings.including(
-                root.title,
-                root.content,
-                root.board,
-                root.createdAt
-        );
-        bindings.bind(root.title).first(StringExpression::containsIgnoreCase);
-        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
-        bindings.bind(root.board).first(SimpleExpression::eq);
-        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
-    }
+    Page<Article> findByBoardAndTitleContainingOrContentContaining(Board board, String title, String content, Pageable pageable);
 }
