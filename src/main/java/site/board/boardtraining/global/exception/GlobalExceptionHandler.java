ae6af8af56a41e14d88954a760518f0d7391e2ba
edgar6bf <edgar6bf@gmail.com>
2023-08-30 15:50:53 +0900

    style: #40 불필요한 import 제거

--- src/main/java/site/board/boardtraining/global/exception/GlobalExceptionHandler.java
+++ src/main/java/site/board/boardtraining/global/exception/GlobalExceptionHandler.java
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import site.board.boardtraining.global.response.error.ErrorApiResponse;
 
-import static site.board.boardtraining.global.exception.GlobalErrorCode.RESOURCE_NOT_FOUND;
-
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
